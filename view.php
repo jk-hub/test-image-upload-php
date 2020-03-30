3632c83833c250eeec33527c42ef115a6a1442c7
jkataria2406 <jkataria2406@gmail.com>
2020-03-30 21:16:42 +0530

    first check in

---
+++ view.php
@@ -0,0 +1,41 @@
+
+<?php
+if(!empty($_GET['id'])){
+    //DB details
+    $dbHost     = 'localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName     = 'cloud';
+    
+    //Create connection and select DB
+    $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+    
+    //Check connection
+    if($db->connect_error){
+       die("Connection failed: " . $db->connect_error);
+    }
+    
+    //Get image data from database
+    $result = $db->query("SELECT image FROM images WHERE id = {$_GET['id']}");
+    
+    if($result->num_rows > 0){
+        $imgData = $result->fetch_assoc();
+        
+        //Render image
+        header("Content-type: image/jpg"); 
+        echo $imgData['image']; 
+    }else{
+        echo 'Image not found...';
+    }
+}
+
+$res = mysql_query("SELECT * from images");
+echo "<table>";
+while($row=mysql_fetch_array($res))
+{
+    echo "<tr>";
+    echo "<td>";
+    <img src = " <? php echo $row ["image"]; ?> " height= "100" width = "100"> 
+    <? php echo "</td>">
+}
+?>
